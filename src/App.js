003ba8c4b1db911f5a5315e76659e1246692521f
jjk <crossfit@a.naver.com>
2020-07-28 17:36:01 +0900

    news-app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    data: {
+      userId: null,
+      id: null,
+      title: null,
+      completed: null,
+    },
+  };
+
+  onClick = async () => {
+    const { data } = this.state;
+    let resp = await Axios.get("https://jsonplaceholder.typicode.com/todos/1");
+    console.log(resp);
+    this.setState({
+      data: { data, ...resp.data },
+    });
+  };
+
+  render() {
+    const { data } = this.state;
+    return (
+      <div>
+        <div>아이디 :{data.userId}</div>
+        <div>제목 :{data.title}</div>
+        <hr />
+        <h1>클릭하세요</h1>
+        <button onClick={this.onClick}>클릭</button>
+      </div>
+    );
+  }
 }
 
 export default App;
